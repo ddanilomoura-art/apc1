b89e34a21a587e503474729906fdb36219ae2af7
unknown <2612082030@ACADEMICO.IESB.BR>
2026-03-11 10:57:34 -0300

    adicionar codigos da aula02

--- aulas/Aula01/ola_mundo.c
+++ aulas/Aula01/ola_mundo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("ola pessoal!");
+    printf("ola mundo!");
     return 0;
 }
---
+++ aulas/Aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  //inclusao de bibliotecas da linguagem
+
+int main() {
+    // 1. entrada
+    
+    // 2. processamento
+    
+    // 3. saida
+    printf("um texto qualquer\n");// \n quebra a linha
+    printf("programar em C eh top!\n");
+    printf("%i\n", 10);
+    printf("%f\n", 10.8748);
+    return 0;
+}
