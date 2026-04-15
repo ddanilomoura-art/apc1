3d75139f0727822d24a1462f835aa79b8c890851
unknown <2612082030@ACADEMICO.IESB.BR>
2026-04-15 10:55:24 -0300

    continuacao pratica 04

--- praticas/pratica04/leia_caracteres.c
+++ praticas/pratica04/leia_caracteres.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    
+    char caractere;
+
+    printf("insira um caractere: ");
+    scanf("%c", &caractere);
+    while (getchar() != '\n');
+
+    printf("%i", caractere);
     return 0;
 }
